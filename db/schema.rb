5b4859a477255fed8a818ef1f64c92b9ae9f8910
kota-kaneta <foruri@icloud.com>
2020-09-23 20:09:09 +0900

    プルダウンの実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_23_072636) do
+ActiveRecord::Schema.define(version: 2020_09_23_080738) do
 
   create_table "room_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "users_id"
-    t.bigint "rooms_id"
+    t.bigint "room_id"
+    t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["rooms_id"], name: "index_room_users_on_rooms_id"
-    t.index ["users_id"], name: "index_room_users_on_users_id"
+    t.index ["room_id"], name: "index_room_users_on_room_id"
+    t.index ["user_id"], name: "index_room_users_on_user_id"
   end
 
   create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -40,6 +40,6 @@ ActiveRecord::Schema.define(version: 2020_09_23_072636) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "room_users", "rooms", column: "rooms_id"
-  add_foreign_key "room_users", "users", column: "users_id"
+  add_foreign_key "room_users", "rooms"
+  add_foreign_key "room_users", "users"
 end
